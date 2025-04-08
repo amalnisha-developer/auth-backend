343f07f541d6b1666cc632087d732ebb716548b2
amalnisha-developer <amalnisha7705@gmail.com>
2025-04-08 18:24:30 +0530

    first commit

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 
 const app = express();
 connectDB();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'], // Vite default port
+    credentials: true
+  }));
+  
 app.use(express.json());
 
 app.get('/',(req,res)=>{
